64d861a17c0f17ce46dc3aeb92599198db383e2b
Tomás Pedro <tpedro@frba.utn.edu.ar>
2024-04-07 20:15:43 -0300

    Add files via upload

---
+++ 00-CHelloWorld/hello.c
@@ -0,0 +1,16 @@
+/*
+    TP0
+    En este TP simplemente imprimimos "Hello, World!" tanto en la terminal como en otro archivo de texto y controlamos salidas.
+    Integrante: Tomás Pedro
+    Día: 07/04/2024
+*/
+
+#include <stdio.h>
+
+int main ()
+{
+    FILE* Archivo;
+    Archivo = fopen ("stdout.txt","w");
+    fprintf (Archivo, "Hello, World!");
+    return 0;
+}
